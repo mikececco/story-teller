6562c0a63304e7c0af83905fb8242b697927c445
Mike Cecconello <mikececco2000@gmail.com>
2024-07-31 09:20:40 +0200

    asChild makes the button a clickable Link

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,21 @@
 import Image from "next/image";
+import Logo from "@/images/logo.png"
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main>
-      <h1>Hello</h1>
+      <section>
+        <div>
+          <Image src={Logo} alt="Logo" height={250}/>
+          <Button asChild>
+            <Link href='/'>
+              Explore
+            </Link>
+          </Button>
+        </div>
+      </section>
     </main>
   );
 }
