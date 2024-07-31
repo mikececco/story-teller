7e4b6f201bf7f789b1d42163062de525fed91634
Mike Cecconello <mikececco2000@gmail.com>
2024-07-31 21:39:07 +0200

    flex-col-reverse to scroll down auto

--- components/StoryWriter.tsx
+++ components/StoryWriter.tsx
@@ -36,7 +36,9 @@ function StoryWriter() {
         </Button>
       </section>
       <section className="flex-1 pb-5 mt-5">
+        <div className="flex flex-col-reverse w-full space-y-2 bg-gray-800 rounded-md text-gray-200 font-mono p-10 h-96 overflow-y-scroll">
 
+        </div>
       </section>
     </div>
   )
