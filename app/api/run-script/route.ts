dc04a7544db97b2117364cab7a08f7c5e3090cd7
Mike Cecconello <mikececco2000@gmail.com>
2024-08-01 23:32:17 +0200

    installed gpt-script with @gptscript-ai/gptscript and set up POST request endpoint + setting instance of GPT + streaming gpt with readable stream backend

---
+++ app/api/run-script/route.ts
@@ -0,0 +1,40 @@
+import { NextRequest } from "next/server";
+import { RunEventType, RunOpts } from "@gptscript-ai/gptscript";
+import g from "@/lib/gpScriptInstance";
+const script = 'app/api/run-script/story-book.gpt'
+
+export async function POST(request: NextRequest) {
+  const { story, pages, path } = await request.json() //extract body of request
+
+  const opts: RunOpts = {
+    disableCache: true,
+    input: `--story ${story} --pages ${pages} --path ${path}`, //arguments example cli commands: gpt-script ./story-book.gpt --story "A robot and a ..." --pages 5 --path ./stories
+  }
+
+  try {
+    const encoder = new TextEncoder();
+    const stream = new ReadableStream({
+      async start(controller) {
+        try {
+          const run = await g.run(script, opts)
+
+          run.on(RunEventType.Event, (data) => {
+            controller.enqueue(encoder.encode( //creating stream
+              `event ${JSON.stringify(data)} \n\n`
+            ))
+          })
+
+          await run.text()
+          controller.close()
+        } catch (error) {
+          console.error(error);
+          console.log('Error', error);
+        }
+      }
+    })
+  } catch (error) {
+    return new Response(JSON.stringify({error: error}), {
+      status: 500
+    })
+  }
+}
